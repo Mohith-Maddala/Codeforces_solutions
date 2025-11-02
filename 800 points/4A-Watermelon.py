79afdc1f57b1bbef0a4f84fc676941c0db9e0169
mohith007 <1577maddala25@gmail.com>
2025-11-02 15:53:13 +0530

    Create 4A-Watermelon.py

---
+++ 800 points/4A-Watermelon.py
@@ -0,0 +1,11 @@
+    #4A
+     
+    weight_of_watermelon = int(input())
+    half_weight = weight_of_watermelon/2
+    if 0<weight_of_watermelon<101:
+        if weight_of_watermelon  == 2:
+            print("NO")
+        elif weight_of_watermelon%2 == 0:
+          print("YES")
+        else:
+            print("No")
